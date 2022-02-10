7817f09b7ac76623e820f16ec3278634866c0078
Lilly Daniell <daniell.lilly@gmail.com>
2022-02-10 16:54:23 +0000

    Add power saving functionality and temperature restrictions to #up method

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -19,5 +19,28 @@ describe('Thermostat', () => {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   })
+
+  it('has a minium temperature of 10 degrees', () => {
+    for (let i = 0; i < 15; i++ ) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10)
+  })
+  it('has power saving mode on by default', () => {
+    expect(thermostat.isPowerSavingModeOn()).toEqual(true)
+  })
+  it('has a maximum temperature of 25 degrees in power save mode', () => {
+    for (let i =0; i < 6; i++ ) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(25)
+  })
+  it('has a maximum temperature of 32 degrees when not in power save mode', () => {
+    thermostat.powerSavingModeOff()
+    for (let i =0; i < 13; i++ ) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(32)
+  })
 });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,14 +3,35 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.MAXIMUM_TEMPERATURE = 32;
+    this.MAXIMUM_POWER_SAVE_TEMPERATURE = 25;
+    this.powerSavingMode = true;
   }
   getCurrentTemperature() { 
     return this.temperature;
   }
   up() {
+    if (this.isPowerSavingModeOn() && this.getCurrentTemperature() < this.MAXIMUM_POWER_SAVE_TEMPERATURE) {
+    this.temperature += 1;
+  } else if (this.isPowerSavingModeOn() === false && this.getCurrentTemperature() < this.MAXIMUM_TEMPERATURE) {
     this.temperature += 1;
   }
+  }
   down() {
+    if (this.isMimumumTemperature()) {
+      return;
+    }
     this.temperature -= 1;
   }
+  isMimumumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+  isPowerSavingModeOn() {
+    return this.powerSavingMode;
+  }
+  powerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+ 
 };
