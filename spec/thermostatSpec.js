b259d9548b1ef62db775819ae46334466484de54
Lilly Daniell <daniell.lilly@gmail.com>
2022-02-10 17:42:58 +0000

    Refactor #up method for SRP and create #isMaximumTemperature method

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -26,21 +26,34 @@ describe('Thermostat', () => {
     }
     expect(thermostat.getCurrentTemperature()).toEqual(10)
   })
-  it('has power saving mode on by default', () => {
-    expect(thermostat.isPowerSavingModeOn()).toEqual(true)
-  })
-  it('has a maximum temperature of 25 degrees in power save mode', () => {
-    for (let i =0; i < 6; i++ ) {
-      thermostat.up();
-    }
-    expect(thermostat.getCurrentTemperature()).toEqual(25)
-  })
-  it('has a maximum temperature of 32 degrees when not in power save mode', () => {
-    thermostat.powerSavingModeOff()
-    for (let i =0; i < 13; i++ ) {
-      thermostat.up();
-    }
-    expect(thermostat.getCurrentTemperature()).toEqual(32)
-  })
+ 
+
+  describe('Power Saving Mode', () => {
+    it('has power saving mode on by default', () => {
+      expect(thermostat.isPowerSavingModeOn()).toEqual(true)
+    })
+
+    it('can switch power saving mode off and on', () => {
+      thermostat.powerSavingModeOff();
+      expect(thermostat.isPowerSavingModeOn()).toEqual(false)
+      thermostat.powerSavingModeOn();
+      expect(thermostat.isPowerSavingModeOn()).toEqual(true)
+    })
+
+    it('has a maximum temperature of 25 degrees in power save mode', () => {
+      for (let i =0; i < 6; i++ ) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25)
+    })
+
+    it('has a maximum temperature of 32 degrees when not in power save mode', () => {
+      thermostat.powerSavingModeOff();
+      for (let i =0; i < 13; i++ ) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32)
+    })
+  });
 });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -8,30 +8,48 @@ class Thermostat {
     this.MAXIMUM_POWER_SAVE_TEMPERATURE = 25;
     this.powerSavingMode = true;
   }
+
   getCurrentTemperature() { 
     return this.temperature;
   }
+
   up() {
-    if (this.isPowerSavingModeOn() && this.getCurrentTemperature() < this.MAXIMUM_POWER_SAVE_TEMPERATURE) {
-    this.temperature += 1;
-  } else if (this.isPowerSavingModeOn() === false && this.getCurrentTemperature() < this.MAXIMUM_TEMPERATURE) {
+    if (this.isMaximumTemperature()) {
+      return;
+    }
     this.temperature += 1;
   }
-  }
+
   down() {
     if (this.isMimumumTemperature()) {
       return;
     }
     this.temperature -= 1;
   }
+
   isMimumumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
   }
-  isPowerSavingModeOn() {
-    return this.powerSavingMode;
+
+  isMaximumTemperature() {
+   return (this.isPowerSavingModeOn() === true && this.getCurrentTemperature() === this.MAXIMUM_POWER_SAVE_TEMPERATURE) || 
+   (this.isPowerSavingModeOn() === false && this.getCurrentTemperature() === this.MAXIMUM_TEMPERATURE)
   }
+  // isMaximumTemperature() {
+  //  if (this.isPowerSavingModeOn() === true) { 
+  //    return this.getCurrentTemperature() === this.MAXIMUM_POWER_SAVE_TEMPERATURE;
+  //  }
+  // }
+
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+
   powerSavingModeOff() {
     this.powerSavingMode = false;
   }
- 
+
+  powerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
 };
